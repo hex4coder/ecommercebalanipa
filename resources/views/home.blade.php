a06fece62af963ae614150955fa2b4fe92f08d3e
hex4coder <the.programmer.luyo@gmail.com>
2024-12-05 13:09:40 +0700

    membuat prototipe persiapan landing page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    {{-- tempat ecommerce --}}
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::view('/', 'home')->name('home');
+Route::view('login', function() {
+    return view('livewire.pages.auth.login');
+});
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+Route::get('kategori', function() {
+    return view('kategori');
+})->name('kategori');
+
+Route::get('brand', function() {
+    return view('brand');
+})->name('brand');
+
+// Route::view('dashboard', 'dashboard')
+//     ->middleware(['auth', 'verified'])
+//     ->name('dashboard');
+
+// Route::view('profile', 'profile')
+//     ->middleware(['auth'])
+//     ->name('profile');
 
 require __DIR__.'/auth.php';
