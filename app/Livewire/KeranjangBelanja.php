f86a0d6df5d15dc2dc27e3b30fcc05f7f2a78d63
hex4coder <the.programmer.luyo@gmail.com>
2024-12-13 08:12:10 +0700

    penambahan tombol kosongkan keranjang belanja

--- app/Livewire/KeranjangBelanja.php
+++ app/Livewire/KeranjangBelanja.php
@@ -3,20 +3,27 @@
 namespace App\Livewire;
 
 use App\Helpers\CartHelper;
-// use Jantinnerezo\LivewireAlert\LivewireAlert;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Attributes\On;
 use Livewire\Component;
 
 class KeranjangBelanja extends Component
 {
-
+    use LivewireAlert;
 
     public $items = [];
     public $totalBiaya = 0;
 
+    public function getListeners()
+    {
+        return [
+            'confirmed'
+        ];
+    }
 
     #[On('cart-updated')]
-    public function mount() {
+    public function mount()
+    {
         $this->items = CartHelper::getCart();
         $this->totalBiaya = CartHelper::getTotal();
     }
@@ -28,13 +35,33 @@ class KeranjangBelanja extends Component
     }
 
 
-    public function AddQty($id) {
+    public function AddQty($id)
+    {
         CartHelper::increment($id);
         $this->dispatch('cart-updated');
-
     }
-    public function DecQty($id) {
+    public function DecQty($id)
+    {
         CartHelper::decrement($id);
         $this->dispatch('cart-updated');
+    }
+
+
+    public function ClearCart()
+    {
+        $this->confirm('Kosongkan keranjang belanja?', [
+            'onConfirmed' => 'confirmed',
+            'cancelButtonText' => 'Tidak',
+            'confirmButtonText' => 'Ya',
+            'confirmButtonColor' => 'red',
+            'cancelButtonColor' => 'blue',
+        ]);
+    }
+
+    public function confirmed()
+    {
+        CartHelper::clearCart();
+        $this->dispatch('cart-updated');
+        $this->alert('success', 'Keranjang dikosongkan.');
     }
 }
