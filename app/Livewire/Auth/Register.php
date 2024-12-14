28add642ff6d17a8899c3990b86d2a5531a73f74
hex4coder <the.programmer.luyo@gmail.com>
2024-12-14 13:10:25 +0700

    prototipe tampilan registrasi user

--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -6,8 +6,21 @@ use Livewire\Component;
 
 class Register extends Component
 {
+
+
+    public $email = '';
+    public $password = '';
+    public $passwordConfirm = '';
+    // TODO: Membuat register user / costumer
+
+
     public function render()
     {
         return view('livewire.auth.register');
+    }
+
+
+    public function register() {
+        // registrasi user
     }
 }
